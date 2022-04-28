595553621aa1d40b0a92a9acc85a9bac5abab960
shravya.angadipeta <shravya.angadipeta@thoughtworks.com>
2022-04-28 10:43:03 +0530

    Added terraform provider file

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  version = "~> 1.0"
+  region  = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "966185979698_Admin-Account-Access"
+}
